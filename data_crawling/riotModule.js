f52161f13d08282e08d8c55d5d5d82f9bdfb08cc
bluejoyq <bluejoyq@naver.com>
2019-09-24 11:04:21 +0900

    use dotenv instead of json

--- data_crawling/riotModule.js
+++ data_crawling/riotModule.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path:'./process.env'})
 const fs = require('fs');
 const rp = require('request-promise')
 
@@ -5,30 +6,20 @@ let riot ={};
 
 const URL = "https://kr.api.riotgames.com";
 
-const HeaderFrame = {
+const HEADER = {
     "Origin": "https://developer.riotgames.com",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "X-Riot-Token": "temp",
+    "X-Riot-Token": process.env.API_KEY,
     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
 };
 
 riot.MakeLink=(query)=>{ 
     return new Promise((resolve,reject)=>{
-        let HEADER = HeaderFrame;
-        fs.readFile('./data_crawling/key.json','utf8',(err,data)=>{
-            if(err){
-                throw(err);
-            }
-            data = JSON.parse(data);
-            HEADER["X-Riot-Token"]=data.key;
-
-            resolve({
-                uri : URL + query,
-                headers : HEADER
-            }); 
-        });
-        
+        resolve({
+            uri : URL + query,
+            headers : HEADER
+        }); 
     });  
 };
 
